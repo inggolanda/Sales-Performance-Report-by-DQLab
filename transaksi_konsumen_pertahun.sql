58a9e7eb92f72d965041af14866b20b2ad8a6679
inggolanda <76269274+inggolanda@users.noreply.github.com>
2022-03-30 08:54:32 +0700

    Deskripsi Project

--- transaksi_konsumen_pertahun.sql
+++ transaksi_konsumen_pertahun.sql
@@ -1,3 +1,5 @@
+--mencari jumlah customer yang bertransaksi setiap tahunnya dari 2009-2012
+
 SELECT YEAR(order_date) AS years, COUNT( DISTINCT customer) AS number_of_customer
 FROM dqlab_sales_store
 WHERE order_status = 'order finished'
