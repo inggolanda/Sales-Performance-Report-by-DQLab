a79b2626b032cad29c79fe01aada4e9aff0b87d7
inggolanda <76269274+inggolanda@users.noreply.github.com>
2022-03-30 08:46:23 +0700

    Deskripsi Project

--- promosi_by_tahun.sql
+++ promosi_by_tahun.sql
@@ -1,3 +1,6 @@
+--Formula untuk burn rate : (total discount / total sales) * 100
+--buat Derived Tables untuk menghitung total sales (sales) dan total discount (promotion_value) berdasarkan tahun(years) dan formulasikan persentase burn rate nya (burn_rate_percentage).
+
 SELECT YEAR(order_date) AS years, SUM(sales) AS sales, SUM(discount_value) AS promotion_value, ROUND((SUM(discount_value)/SUM(sales)) * 100, 2) AS burn_rate_percentage
 FROM dqlab_sales_store
 WHERE order_status = 'order finished'
