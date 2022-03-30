228c77b632b7cad74a1c8cb817ddd07fd4b73755
inggolanda <76269274+inggolanda@users.noreply.github.com>
2022-03-30 08:52:50 +0700

    Deskripsi Project

--- promosi_by_productsubcategory.sql
+++ promosi_by_productsubcategory.sql
@@ -1,4 +1,12 @@
-SELECT YEAR(order_date) AS years, product_sub_category, product_category, SUM(sales) AS sales, SUM(discount_value) AS promotion_value, ROUND((SUM(discount_value)/SUM(sales)) * 100, 2) AS burn_rate_percentage
+--Analisa terhadap efektifitas dan efisiensi dari promosi yang sudah dilakukan selama 2012
+
+SELECT 
+YEAR(order_date) AS years, 
+product_sub_category, 
+product_category, 
+SUM(sales) AS sales, 
+SUM(discount_value) AS promotion_value, 
+ROUND((SUM(discount_value)/SUM(sales)) * 100, 2) AS burn_rate_percentage
 FROM dqlab_sales_store
 WHERE order_status = 'order finished' AND YEAR(order_date) = 2012
 GROUP BY years, product_sub_category, product_category
