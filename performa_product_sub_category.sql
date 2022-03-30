766eb0a4fd20f3eb805d7abf54fce7984a19834e
inggolanda <76269274+inggolanda@users.noreply.github.com>
2022-03-30 08:42:46 +0700

    Deskripsi Project

--- performa_product_sub_category.sql
+++ performa_product_sub_category.sql
@@ -1,3 +1,5 @@
+--mencari total penjualan (sales) berdasarkan sub category dari produk (product_sub_category) pada tahun 2011 dan 2012 saja (years) 
+
 SELECT YEAR (order_date) AS years, product_sub_category , SUM(sales) AS sales
 FROM dqlab_sales_store
 WHERE order_status = 'order finished' AND YEAR(order_date) > 2010
