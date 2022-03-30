25a24d5ccdb6b6bfe44e0fde2cd49a327b315dbd
inggolanda <76269274+inggolanda@users.noreply.github.com>
2022-03-30 08:14:35 +0700

    Menambahkan Deskripsi

--- performa_dqlab.sql
+++ performa_dqlab.sql
@@ -1,3 +1,5 @@
+--mencari total penjualan (sales) dan jumlah order (number_of_order) dari tahun 2009 sampai 2012 (years). 
+
 select YEAR(order_date) AS years,sum(Sales) AS sales, count(order_quantity) AS number_of_order
 from dqlab_sales_store
 WHERE order_status = "order finished"
